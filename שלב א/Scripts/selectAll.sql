6592f1aeece5eb9ebd851cc40a733191bbe6c8f1
dandanseb <dansebbah2@gmail.com>
2025-04-07 22:50:10 +0300

    Update selectAll.sql

--- שלב א/Scripts/selectAll.sql
+++ שלב א/Scripts/selectAll.sql
@@ -1,26 +1,29 @@
--- Select all data from Creators
-SELECT * FROM Creators;
-
--- Select all data from CreatorContent
-SELECT * FROM CreatorContent;
-
--- Select all data from CreatorRoles
-SELECT * FROM CreatorRoles;
+-- Select all data from Roles
+SELECT * FROM Roles;
 
 -- Select all data from Awards
 SELECT * FROM Awards;
 
--- Select all data from CreatorRating
-SELECT * FROM CreatorRating;
+-- Select all data from Collaboration
+SELECT * FROM Collaboration;
 
--- Select all data from CreatorContracts
-SELECT * FROM CreatorContracts;
-
--- Select all data from CreatorEarnings
-SELECT * FROM CreatorEarnings;
-
--- Select all data from CreatorCollab
-SELECT * FROM CreatorCollab;
+-- Select all data from Creators
+SELECT * FROM Creators;
 
 -- Select all data from Contents
 SELECT * FROM Contents;
+
+-- Select all data from Rating
+SELECT * FROM Rating;
+
+-- Select all data from Earnings
+SELECT * FROM Earnings;
+
+-- Select all data from Contracts
+SELECT * FROM Contracts;
+
+-- Select all data from Receives_award
+SELECT * FROM Receives_award;
+
+-- Select all data from Collaborate_with
+SELECT * FROM Collaborate_with;
