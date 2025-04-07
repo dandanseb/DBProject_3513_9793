9b825a35934251714e736ee3be9830afdac480c4
dandanseb <dansebbah2@gmail.com>
2025-04-07 22:40:45 +0300

    Create selectAll.sql

---
+++ שלב א/Scripts/selectAll.sql
@@ -0,0 +1,26 @@
+-- Select all data from Creators
+SELECT * FROM Creators;
+
+-- Select all data from CreatorContent
+SELECT * FROM CreatorContent;
+
+-- Select all data from CreatorRoles
+SELECT * FROM CreatorRoles;
+
+-- Select all data from Awards
+SELECT * FROM Awards;
+
+-- Select all data from CreatorRating
+SELECT * FROM CreatorRating;
+
+-- Select all data from CreatorContracts
+SELECT * FROM CreatorContracts;
+
+-- Select all data from CreatorEarnings
+SELECT * FROM CreatorEarnings;
+
+-- Select all data from CreatorCollab
+SELECT * FROM CreatorCollab;
+
+-- Select all data from Contents
+SELECT * FROM Contents;
